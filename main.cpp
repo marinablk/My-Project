65d50d54ee28f70abd14e873fd515fe54d7373e6
Belyakova Marina <belyakovam99@gmail.com>
2024-12-24 00:49:03 +0300

    Добавлен вывод умножения

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main() {
 	std::cin >> a >> b; 
 	std::cout << "a + b = " << a+b << std::endl;
 	std::cout << "a - b = " << a-b << std::endl;
+	std::cout << "a * b = " << a*b << std::endl;
 	return 0;
 }
